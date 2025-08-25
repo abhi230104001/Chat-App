c87d2e6601584c2153e2867fdb6892a6223778ea
abhi230104001 <abhishek2304001@gmail.com>
2025-08-25 23:30:20 +0530

    chat app

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { useChatStore } from "../store/useChatStore";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
-import ChatHeader from "./chatHeader";
+import ChatHeader from "./ChatHeader";
 import { formatMessageTime } from "../lib/utils";
 import { useAuthStore } from "../store/useAuthStore";
 
