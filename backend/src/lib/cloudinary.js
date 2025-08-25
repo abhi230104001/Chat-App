ecf9b65e611a884097ae4c955e033b38d7da49e9
abhi230104001 <abhishek2304001@gmail.com>
2025-08-25 22:52:55 +0530

    chatApp

---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,18 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import { config } from "dotenv";
+
+config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+console.log("Cloudinary Config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY ? "OK" : "MISSING",
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "OK" : "MISSING"
+});
+
+export default cloudinary;
